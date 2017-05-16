92030659cdf345e47a89c03798279c33d6613a2b
michael mason <michael@michaeljmason.com>
2017-05-16 10:55:12 -0700

    committing slider from yesterday

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -21,58 +21,21 @@
         // JavaScript to be fired on all pages
 
         $(document).ready(function() {
-            $(".hero--slider").lightSlider({
-                item: 1,
-                autoWidth: false,
-                slideMove: 1, // slidemove will be 1 if loop is true
-                slideMargin: 10,
 
-                addClass: '',
-                mode: "slide",
-                useCSS: true,
-                cssEasing: 'ease', //'cubic-bezier(0.25, 0, 0.25, 1)',//
-                easing: 'linear', //'for jquery animation',////
+          $( '#my-slider' ).sliderPro({
+              width: 1920,
+              height: $(window).height(),
+              autoplay: false,
+              loop: false,
+              slideAnimationDuration: 1500,
+              orientation: 'horizontal',
+              fade: true,
+              fadeDuration: 1000,
+              fadeArrows: true,
+              arrows: true,
+              buttons: true
+          });            
 
-                speed: 400, //ms'
-                auto: false,
-                pauseOnHover: false,
-                loop: false,
-                slideEndAnimation: true,
-                pause: 2000,
-
-                keyPress: false,
-                controls: true,
-                prevHtml: '',
-                nextHtml: '',
-
-                rtl:false,
-                adaptiveHeight:false,
-
-                vertical:false,
-                verticalHeight:500,
-                vThumbWidth:100,
-
-                thumbItem:10,
-                pager: true,
-                gallery: false,
-                galleryMargin: 5,
-                thumbMargin: 5,
-                currentPagerPosition: 'middle',
-
-                enableTouch:true,
-                enableDrag:true,
-                freeMove:true,
-                swipeThreshold: 40,
-
-                responsive : [],
-
-                onBeforeStart: function (el) {},
-                onSliderLoad: function (el) {},
-                onBeforeSlide: function (el) {},
-                onAfterSlide: function (el) {},
-                onBeforeNextSlide: function (el) {},
-                onBeforePrevSlide: function (el) {}
-            });
         });
 
       },
