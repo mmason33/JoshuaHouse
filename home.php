b920211342d51112c84a1cdff8d3938b0613ecf8
michael mason <michael@michaeljmason.com>
2017-07-27 14:24:37 -0700

    ignore

--- home.php
+++ home.php
@@ -11,7 +11,8 @@
       <div class="coming-soon">
         <img src="/wp-content/uploads/2017/05/j-house-logo-big.png" class="img-fluid">
         <hr>
-        <h2>Coming Soon</h2>
+        <h4>Coming Soon</h4>
+        <a href="tel:8663110003">866.311.0003</a>
       </div>
     </div>
   </div>
